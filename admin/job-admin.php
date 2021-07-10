c6223ca8b7540b4db6f0781a078d01f31995820e
Desmond Bennett <info@dpbennett.com.jm>
2021-07-10 15:26:55 -0500

    General updates.

--- admin/job-admin.php
+++ admin/job-admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Job Administration...
  */
@@ -25,8 +26,8 @@ function jmts_create_job_post_type() {
                     'parent' => 'Parent Job'
                 ),
                 'public' => true,
-                'menu_position' => 20,
-                'supports' => array('title', 'editor', 'comments', 'thumbnail'),
+                //'menu_position' => 20,
+                'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
                 'taxonomies' => array(''),
                 //'menu_icon' => plugins_url('images/jmts.png', __FILE__),
                 'has_archive' => true,
@@ -34,92 +35,3 @@ function jmts_create_job_post_type() {
             )
     );
 }
-
-//add_action('admin_init', 'jmts_job_br_admin_init');
-
-function jmts_job_br_admin_init() {
-    $var1 = 'this';
-    $var2 = 'that';
-    add_meta_box('jmts_job_details_meta_box',
-            'Job Details',
-            'jmts_br_display_job_details_meta_box',
-            'job');
-}
-
-//function jmts_job_br_admin_init() {
-//    add_meta_box('jmts_job_details_meta_box',
-//            'Job Details',
-//            'jmts_br_display_job_details_meta_box',
-//            'job', 
-//            'normal', 
-//            'high');
-//}
-
-function jmts_br_display_job_details_meta_box($job) {
-// Retrieve current author and rating based on review ID
-    $job_number = esc_html(get_post_meta($job->ID, 'job_number', true));
-    //$job_tat = intval(get_post_meta($job->ID,'job_tat', true));
-    ?>
-    <table>
-        <tr>
-            <td style="width: 100%">
-                <strong>Job Number</strong>
-            </td>
-            <td>
-                <input type="text" size="80"
-                       name="job_number"
-                       value="<?php echo $job_number; ?>" />
-            </td>
-        </tr>        
-    </table> 
-    <?php
-}
-
-//add_action('save_post', 'jmts_br_add_job_fields', 10, 2);
-
-function jmts_br_add_job_fields($job_id, $job) {
-    // Check post type for jobs
-    if ('jobs' == $job->post_type) {
-        // Store data in post meta table if present in post data
-        if (isset($_POST['job_number'])) {
-            update_post_meta($job_id, 'job_number',
-                    sanitize_text_field(
-                            $_POST['job_number']));
-        }
-    }
-}
-
-//add_filter('template_include', 'ch4_br_template_include', 1);
-
-function ch4_br_template_include($template_path) {
-    if ('job' == get_post_type()) {
-        if (is_single()) {
-// checks if the file exists in the theme first,
-// otherwise install content filter
-            if ($theme_file = locate_template(array
-                ('single-job.php'))) {
-                $template_path = $theme_file;
-            } else {
-                add_filter('the_content',
-                        'jmts_br_display_single_job',
-                        20);
-            }
-        }
-    }
-    return $template_path;
-}
-
-function jmts_br_display_single_job($content) {
-    if (!empty(get_the_ID())) {
-        echo 'yes!!!';
-    }
-}
-
-// tk Adds a new custom field if the key does not already exist, or updates the 
-// value of the custom field with that key otherwise.
-//if ( ! add_post_meta( 7, 'fruit', 'banana', true ) ) { 
-//   update_post_meta ( 7, 'fruit', 'banana' );
-//}
-
-
-
--- public/job/job.php
+++ public/job/job.php
@@ -3,22 +3,67 @@
 add_shortcode('job-list', 'jmts_job_list');
 
 function jmts_job_list() {
-// Preparation of query array to retrieve 5 book reviews
+    if ( isset( $_GET['job_id'] ) ) {
+        echo '<h2>Job Detail</h2>';
+        echo $_GET['job_id'];
+        
+        echo "<br><br><a href=\"javascript:history.go(-1)\">Go back to job list</a>";
+        
+        exit();
+    }
+    
+    // Preparation of query array to retrieve 5 jobs
     $query_params = array('post_type' => 'job',
         'post_status' => 'publish',
         'posts_per_page' => 5);
-// Execution of post query
-    $job_query = new WP_Query;
-    $job_query->query($query_params);
-// Check if any posts were returned by the query
-    if ($job_query->have_posts()) {
-        echo 'Jobs found!!';
-        
-        while ($job_query->have_posts()) {
-            $job_query->the_post();
-            echo get_the_title( get_the_ID() );//get_permalink();
-        }
+
+    // Retrieve page query variable, if present
+    $page_num = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+
+    // If page number is higher than 1, add to query array
+    if ($page_num != 1) {
+        $query_params['paged'] = $page_num;
     }
 
-    wp_reset_postdata();
+    // Execution of post query
+    $job_query = new WP_Query;
+    $job_query->query($query_params);
+
+    // Check if any posts were returned by query
+    if ($job_query->have_posts()) {
+        // Display posts in table layout
+        $output = '<h2>Job List</h2>';
+        $output .= '<table>';
+        $output .= '<tr><th style="width: 350px"><strong>Title</strong></th>';
+        $output .= '<th><strong>Job Number</strong></th></tr>';
+
+        // Cycle through all items retrieved
+        while ($job_query->have_posts()) {
+            $job_query->the_post();
+            $output .= '<tr><td><a href="' . './?job_id='. get_the_ID() . '">';
+            $output .= get_the_title(get_the_ID()) . '</a></td>';
+            $output .= '<td>' . esc_html(get_post_meta(get_the_ID(), 'job_number', true));
+            $output .= '</td></tr>';
+        }
+
+        $output .= '</table>';
+
+        // Display page navigation links
+        if ($job_query->max_num_pages > 1) {
+            $output .= '<nav id="nav-below">';
+            $output .= '<div class="nav-previous">';
+            $output .= get_next_posts_link('<span class="meta-nav">&larr;</span> Older jobs', $job_query->max_num_pages);
+            $output .= '</div>';
+            $output .= "<div class='nav-next'>";
+            $output .= get_previous_posts_link('Newer jobs <span class="meta-nav">&rarr;</span>', $job_query->max_num_pages);
+            $output .= '</div>';
+            $output .= '</nav>';
+        }
+
+        // Reset post data query
+        wp_reset_postdata();
+    }
+
+    return $output;
 }
+
